8c6bb88cae57705008217f0313183abed5ab163c
Celeste <529453864@qq.com>
2020-08-25 02:00:05 +0800

    剑指 Offer 41. 数据流中的中位数

--- src/offer0824/Test.java
+++ src/offer0824/Test.java
@@ -1,5 +1,7 @@
 package offer0824;
 
+import offer0825.MedianFinder;
+
 /**
  * @author: celeste
  * @create: 2020-08-24 01:42
@@ -7,7 +9,13 @@ package offer0824;
  **/
 public class Test {
     public static void main(String[] args) {
-        Permutation pe = new Permutation();
-        pe.permutation1("abc");
+//        Permutation pe = new Permutation();
+//        pe.permutation1("abc");
+        MedianFinder me = new MedianFinder();
+        me.addNum(-1);
+        me.addNum(-2);
+        double i = me.findMedian();
+        me.addNum(-3);
+        me.findMedian();
     }
 }
